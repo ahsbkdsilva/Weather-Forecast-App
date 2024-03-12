aa61fabf69944f2f8f27d016310b43806ddabf72
ahsbkdsilva <96135295+ahsbkdsilva@users.noreply.github.com>
2024-03-13 00:51:57 +0530

    Edit Loging page

--- login.js
+++ login.js
@@ -1,15 +1,14 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("login-form").addEventListener("submit", function(event) {
+        event.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
 
-    
-    if (username === "Sasini" && password === "nanosoft") {
-
-        window.location.href = "index.html";
-    } else {
-        
-        document.getElementById("login-error").innerText = "Invalid username or password.";
-    }
+        if (username === "Sasini" && password === "nanosoft") {
+            window.location.href = "index.html";
+        } else {
+            document.getElementById("login-error").innerText = "Invalid username or password.";
+        }
+    });
 });
